429fdcef06d74fb83108341a9a33b3341e3b48ea
fireraccoon <willyjames.music@gmail.com>
2015-01-14 21:27:07 -0500

    Code refractor

--- core/src/com/brm/GoatEngine/Viewport.java
+++ core/src/com/brm/GoatEngine/Viewport.java
@@ -2,6 +2,7 @@ package com.brm.GoatEngine;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.brm.GoatEngine.ECS.Entity.Entity;
@@ -24,8 +25,14 @@ public class Viewport {
     private float topHostpot    = 0.0f;   // Maximum Y
     private float bottomHotspot = 0.0f;   // Minimum Y
 
-    //For Smooth camera movement(delayed camera movement, (the higher the most direct))
-    private Vector2 speed = new Vector2();
+
+    //For Smooth camera movement(delayed camera movement, (the higher the most direct and quick))
+    private Vector2 speed = new Vector2(8.0f, 5.0f);
+
+    //Zoom parameter
+    float minimumZoom = 0.5f; //The minimum value the camera can Zoom In/Out ==> 1 = default Viewport width value (no Zoom)
+    float maximumZoom = 1.5f; //The minimum value the camera can Zoom In/Out
+
 
     /**
      * @param width width of the area to cover
@@ -42,8 +49,6 @@ public class Viewport {
         this.camera.position.set(camera.viewportWidth/2f, camera.viewportHeight/2f, 0);
         this.camera.update();
 
-        this.speed.x = 8.0f;
-        this.speed.y = 5.0f;
     }
 
 
@@ -52,46 +57,61 @@ public class Viewport {
      * @param targets: The gameObjects having a CameraTargetComponent
      */
     public void update(ArrayList<Entity> targets){
-        Vector2 min = new Vector2(2500,2500);
-        Vector2 max = new Vector2(-2500,-2500);
+        Vector2 leftMost = new Vector2(2500,2500);
+        Vector2 rightMost = new Vector2(-2500,-2500);
 
-        //Find the Min and MAx positions of entity
+        //Find the left most entity and the right most entity
         for(Entity target : targets){
             PhysicsComponent phys = (PhysicsComponent) target.getComponent(PhysicsComponent.ID);
 
-            min.x = Math.min(min.x, phys.getPosition().x);
-            min.y = Math.min(min.y, phys.getPosition().y);
+            leftMost.x = Math.min(leftMost.x, phys.getPosition().x);
+            leftMost.y = Math.min(leftMost.y, phys.getPosition().y);
 
-            max.x = Math.max(max.x, phys.getPosition().x);
-            max.y = Math.max(max.y, phys.getPosition().y);
+            rightMost.x = Math.max(rightMost.x, phys.getPosition().x);
+            rightMost.y = Math.max(rightMost.y, phys.getPosition().y);
         }
 
+        // Find the center point between between the two entities
+        updatePosition(leftMost, rightMost);
 
-        // Find the center point between min and max pos
+        //ZOOM IN/OUT according to the distance between the two entities
+        updateZoom(leftMost, rightMost);
+
+        this.getCamera().update();
+    }
+
+    /**
+     * Updates the position of the camera according to the leftMost entity (minPosition)
+     * And the right most entity (maxPosition) so it is placed between to two positions
+     * @param leftMost
+     * @param rightMost
+     */
+    public void updatePosition(Vector2 leftMost, Vector2 rightMost){
+        // Find the center point between leftMost and rightMost pos
         float w = Gdx.graphics.getWidth();
         float h = Gdx.graphics.getHeight();
-        float width = max.x - min.x;
-        float height = (max.y - min.y) * (h/w);
-        Vector2 newPos = new Vector2(width/2+min.x, height/2+min.y);
-
+        float width = rightMost.x - leftMost.x;
+        float height = (rightMost.y - leftMost.y) * (h/w);
+        Vector2 newPos = new Vector2(width/2+leftMost.x, height/2+leftMost.y);
 
         //Reposition the camera to that center point
         Vector3 cameraPosition = getCamera().position;
         cameraPosition.x += (newPos.x - cameraPosition.x) * speed.x * Gdx.graphics.getDeltaTime();
         cameraPosition.y += (newPos.y - cameraPosition.y) * speed.y * Gdx.graphics.getDeltaTime();
+    }
 
 
-        //ZOOM IN/OUT according to the distance between the two entities
+     /**
+     * Updates the Zoom of the camera according to the leftMost entity (minPosition)
+     * And the right most entity (maxPosition) so both position are visible to the camera
+     * @param leftMost
+     * @param rightMost
+     */
+    public void updateZoom(Vector2 leftMost, Vector2 rightMost){
         //Pythagorean distance
-        Logger.log(VectorMath.distance(min, max));
-        float newZoom = (float) (VectorMath.distance(min, max)/10.0f);
-        double speedHyp = Math.sqrt(speed.x*speed.x + speed.y*speed.y);
-        camera.zoom += (newZoom-camera.zoom) * speedHyp * Gdx.graphics.getDeltaTime();
-        Logger.log(newZoom);
-
-
-
-        this.getCamera().update();
+        float zoomScale = (float) (VectorMath.distance(leftMost, rightMost)/10.0f);
+        double zoomSpeed = Math.sqrt(speed.x*speed.x + speed.y * speed.y);
+        camera.zoom += (Math.min(Math.max(zoomScale, this.minimumZoom), this.maximumZoom ) - camera.zoom) * zoomSpeed * Gdx.graphics.getDeltaTime();
     }
 
 
