a24ef7a613efb34a4b37308637f82ec3be6c6c63
fireraccoon <willyjames.music@gmail.com>
2015-05-13 00:31:40 -0400

    Beginning of Spawning Bonus

--- core/src/com/brm/Kubotz/Systems/PowerUpsSystem.java
+++ core/src/com/brm/Kubotz/Systems/PowerUpsSystem.java
@@ -1,13 +1,19 @@
 package com.brm.Kubotz.Systems;
 
+import com.badlogic.gdx.math.MathUtils;
 import com.brm.GoatEngine.ECS.Entity.Entity;
 import com.brm.GoatEngine.ECS.Entity.EntityManager;
 import com.brm.GoatEngine.ECS.System.EntitySystem;
+import com.brm.GoatEngine.Utils.Timer;
 import com.brm.Kubotz.Component.Powerups.PowerUp;
+import com.brm.Kubotz.Component.Powerups.PowerUpComponent;
 import com.brm.Kubotz.Component.Powerups.PowerUpsContainerComponent;
+import com.brm.Kubotz.Constants;
 
 /**
  * Processes the PowerUps of all the entities
+ * It is also responsible of spawning Bonus Objects
+ * randomly
  */
 public class PowerUpsSystem extends EntitySystem {
 
@@ -16,10 +22,27 @@ public class PowerUpsSystem extends EntitySystem {
     }
 
 
+    private Timer spawnTimer; //Timer responsible to determine whether or not we should spawn a new power up
+
+
+    @Override
+    public void init() {
+        super.init();
+        int delay = MathUtils.random(Constants.MIN_DELAY_BONUS_SPAWN, Constants.MAX_DELAY_BONUS_SPAWN);
+        spawnTimer = new Timer(delay);
+        spawnTimer.start();
+    }
+
+    public void update(){
+        updatePowerUps();
+        updateSpawns();
+    }
+
+
     /**
      * Terminates all PowerUps if needed
      */
-    public void update(){
+    private void updatePowerUps(){
         for(Entity entity: em.getEntitiesWithComponent(PowerUpsContainerComponent.ID)){
             PowerUpsContainerComponent container;
             container = (PowerUpsContainerComponent) entity.getComponent(PowerUpsContainerComponent.ID);
@@ -30,6 +53,27 @@ public class PowerUpsSystem extends EntitySystem {
                 }
             }
         }
+    }
+
+    /**
+     * Checks if a new Bonus needs to be spawn
+     * if so spawns one
+     */
+    private void updateSpawns(){
+        if(this.spawnTimer.isDone()){
+            this.spawnTimer.setDelay(MathUtils.random(Constants.MIN_DELAY_BONUS_SPAWN, Constants.MAX_DELAY_BONUS_SPAWN));
+            if(em.getEntitiesWithComponent(PowerUpComponent.ID).size() < Constants.MAX_NB_BONUS){
+                this.spawnPowerUp();
+            }
+        }
+    }
+
+
+    /**
+     * Spawns a new PowerUp
+     */
+    private void spawnPowerUp(){
+       //TODO Make a Random Bonus
     }
 
 
