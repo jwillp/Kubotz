b9251f7377d761b450251e10d389de8104e2f2af
fireraccoon <willyjames.music@gmail.com>
2015-06-06 01:14:44 -0400

    Take Damage for Melee Weapon (kick, punch)

--- core/src/com/brm/Kubotz/Systems/AttackSystems/MeleeSystem.java
+++ core/src/com/brm/Kubotz/Systems/AttackSystems/MeleeSystem.java
@@ -4,13 +4,15 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
-import com.brm.GoatEngine.ECS.utils.Components.PhysicsComponent;
 import com.brm.GoatEngine.ECS.core.Entity.Entity;
+import com.brm.GoatEngine.ECS.core.Entity.Event;
 import com.brm.GoatEngine.ECS.core.Systems.EntitySystem;
+import com.brm.GoatEngine.ECS.utils.Components.PhysicsComponent;
 import com.brm.GoatEngine.Input.VirtualGamePad;
-import com.brm.GoatEngine.Utils.Logger;
 import com.brm.Kubotz.Components.MeleeComponent;
 import com.brm.Kubotz.Constants;
+import com.brm.Kubotz.Events.CollisionEvent;
+import com.brm.Kubotz.Events.TakeDamageEvent;
 import com.brm.Kubotz.Input.GameButton;
 
 /**
@@ -45,6 +47,7 @@ public class MeleeSystem extends EntitySystem{
             if(meleeComponent.getCooldown().isDone() && meleeComponent.getDurationTimer().isDone()){
                 meleeComponent.getDurationTimer().reset();
                 createAttackBox(phys);
+                // TODO FIRE EVENT
             }
         }
     }
@@ -63,6 +66,45 @@ public class MeleeSystem extends EntitySystem{
             }
         }
     }
+
+
+    @Override
+    public <T extends Event> void onEvent(T event) {
+        if(event.getClass() == CollisionEvent.class){
+            onCollision((CollisionEvent) event);
+        }
+    }
+
+
+    /**
+     * Calls when a collision occurs
+     * tries to find it something collided with a
+     * melee attack box
+     * @param e
+     */
+    private void onCollision(CollisionEvent e){
+        if(e.getDescriber() == CollisionEvent.Describer.END){
+            return;
+        }
+        if(e.getEntityA() == null || e.getEntityB() == null){
+                return;
+        }
+
+        if(e.getFixtureA().getUserData().equals(Constants.FIXTURE_MELEE_ATTACK)){
+            MeleeComponent comp = (MeleeComponent) e.getEntityA().getComponent(MeleeComponent.ID);
+
+            //FIRE A TAKE DAMAGE EVENT
+            this.fireEvent(new TakeDamageEvent(
+                    e.getEntityB().getID(),
+                    e.getEntityA().getID(),
+                    comp.getDamage(),
+                    comp.getKnockBack()
+                    )
+            );
+        }
+
+    }
+
 
 
     /**
